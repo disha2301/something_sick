5b09d1c058c7d36c284e3c8b7b68a8353ab7aaf2
Disha <dishay166@gmail.com>
2023-09-01 17:15:02 +0530

    chore: add fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from "next/app";
 import "@/scss/index.scss";
+import { Raleway, Fira_Code } from "next/font/google";
 
+const raleway = Raleway({ subsets: ["latin"] });
+const firaCode = Fira_Code({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
